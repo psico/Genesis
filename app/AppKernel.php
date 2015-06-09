90cc0f20cc3205eb78c4bcdd475cbfba6f877294
João Gabriel dos Santos Rodrigues <desenvolvedorjg@gmail.com>
2015-06-09 16:41:13 -0300

    [UPT] Atualizado versão do jQuery

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Genesis\CoreBundle\GenesisCoreBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
